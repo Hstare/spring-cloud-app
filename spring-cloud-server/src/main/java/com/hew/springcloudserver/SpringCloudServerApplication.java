a0aaf9880a9420f89d1bff5798e377ee21e81cdb
hexiaowei <1278313308@qq.com>
2019-12-08 17:08:46 +0800

    test

--- spring-cloud-server/src/main/java/com/hew/springcloudserver/SpringCloudServerApplication.java
+++ spring-cloud-server/src/main/java/com/hew/springcloudserver/SpringCloudServerApplication.java
@@ -1,13 +1,17 @@
 package com.hew.springcloudserver;
 
+import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * @author Administrator
  */
 @SpringBootApplication
+@EnableDiscoveryClient
+//@EnableDubbo
 public class SpringCloudServerApplication {
 
     public static void main(String[] args) {
